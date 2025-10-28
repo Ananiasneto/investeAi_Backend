1ad0939770cfb9f9d05ee9684d41555fdd42ff75
Ananias <netimcunha@gmail.com>
2025-10-28 15:12:17 -0300

    chore (backend):setup para backend com typescript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { json, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(json());
+
+app.get("/health", (req: Request, res: Response) => {
+    res.status(200).send("ok!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
