ef2820e07c9a5d4ca1da5ab5378d1b2272f02749
Ananias <netimcunha@gmail.com>
2025-10-28 18:57:27 -0300

    fix (backend):instalar cors para resolver problema do frontend

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { json, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors"; 
 import authRouter from "./router/auth-router";
 import homeRouter from "./router/home-router";
 import operationRouter from "./router/operation-router";
@@ -7,11 +8,13 @@ import ativeRouter from "./router/ative-router";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(json());
 app.use(authRouter)
 app.use(homeRouter)
 app.use(operationRouter)
 app.use(ativeRouter)
+
 app.get("/health", (req: Request, res: Response) => {
     res.status(200).send("ok!");
 });
